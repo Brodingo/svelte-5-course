b36322cf32f0e67875f258e36234cf845df1418a
Brodingo <brodyk@gmail.com>
2024-10-21 13:12:18 -0400

    iteration and multistep form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,61 @@
     import Header from './Header.svelte'
 
     let formState = $state({
-        name: '',
-        birthday: '',
+        answers: {},
         step: 0,
         error: ''
     });
 
+    const QUESTIONS = [
+        {
+            question: "What is your name?",
+            id: 'name',
+            type: 'text'
+        },
+        {
+            question: "What is your birthday?",
+            id: 'birthday',
+            type: 'date'
+
+        },
+        {
+            question: "What's your favorite color",
+            id: 'color',
+            type: 'color'
+        }
+    ];
+
+    function nextStep(id: string) {
+        if (formState.answers[id]) {
+            formState.step += 1;
+            formState.error = "";
+        } else {
+            formState.error = "Please fill out the form input";
+        }
+    }
+
 </script>
 
-<Header name={formState.name} />
+<Header name={formState.answers.name} />
 
 <main>
+    {#if formState.step >= QUESTIONS.length}
+        <p>Thank you!</p>
+    {/if}
     <p>Step: {formState.step + 1}</p>
 
-    {@render formStep({
-        question: "What's your name",
-        id: 'name',
-        type: 'text'
-    })}
+    {#each QUESTIONS as question, index (question.id)}
+        {#if formState.step === index}
+        {@render formStep(question)}        
+        {/if}
+    {/each}
+
+    {#if formState.error}
+        <p class="error">{formState.error}</p>
+    {/if}
 </main>
+
+{JSON.stringify(formState)}
 
 {#snippet formStep({question, id, type} : {
     type: string; id: string; question: string;
@@ -28,8 +64,9 @@
     <article>
         <div>
             <label for="{id}">{question}</label>
-            <input {type} {id} bind:value={formState[id]}>
+            <input {type} {id} bind:value={formState.answers[id]}>
         </div>
+        <button onclick="{() => nextStep(id)}">Next</button>
     </article>
 {/snippet}
 
